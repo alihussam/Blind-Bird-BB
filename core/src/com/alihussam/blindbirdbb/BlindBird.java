0e6b0b0c249b81658fd962c4a47298ece0bd3ca6
alihussam <alihussam.cs@gmail.com>
2018-09-15 13:03:49 +0500

    Add collision detection

--- core/src/com/alihussam/blindbirdbb/BlindBird.java
+++ core/src/com/alihussam/blindbirdbb/BlindBird.java
@@ -2,11 +2,14 @@ package com.alihussam.blindbirdbb;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
+
 
 public class BlindBird extends ApplicationAdapter {
 	private SpriteBatch batch;
@@ -16,12 +19,15 @@ public class BlindBird extends ApplicationAdapter {
 	private Texture bottomTube;
 	private int flapState = 0;
 	private float gap = 400;
-	private int gameState = 0; //0 means not playing or at start
+	private int gameState = 0; //0 means not playing
     private float velocity = 0;
     private double gravity = 1.5;
 	private float birdY;
 	private float birdX;
-	private float maxTubeOffset;
+	//private ShapeRenderer shapeRenderer;
+	private Circle birdCircle;
+	private Rectangle[] topTubeRectangles;
+	private Rectangle[] bottomTubeRectangles;
 
 	private float tubeVelocity = 4;
 	private int noOfTubes = 4;
@@ -41,13 +47,18 @@ public class BlindBird extends ApplicationAdapter {
 		bottomTube = new Texture("bottomtube.png");
 		birdX = Gdx.graphics.getWidth()/2 - birds[0].getWidth()/2;
 		birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
-	    maxTubeOffset = Gdx.graphics.getHeight()/2 - gap/2 - 100;
 	    distanceBetweenTubes = Gdx.graphics.getWidth()/2;
         randomGenerator = new Random();
+        topTubeRectangles = new Rectangle[noOfTubes];
+        bottomTubeRectangles = new Rectangle[noOfTubes];
 	    for(int i=0; i < noOfTubes ;i++){
             tubeOffset[i] = (randomGenerator.nextFloat()-0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-            tubeX[i] = Gdx.graphics.getWidth()/2 - topTube.getWidth()/2 + i*distanceBetweenTubes;
+            tubeX[i] = Gdx.graphics.getWidth()/2 - topTube.getWidth()/2 + Gdx.graphics.getWidth() + i*distanceBetweenTubes;
 	    }
+	    //shapeRenderer = new ShapeRenderer();
+	    birdCircle = new Circle();
+
+
 	}
 
 	@Override
@@ -69,6 +80,11 @@ public class BlindBird extends ApplicationAdapter {
                 }
                 batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight()/2 + gap/2 + tubeOffset[i]);
                 batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight()/2 - gap/2 - bottomTube.getHeight() + tubeOffset[i]);
+                topTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight()/2 + gap/2 + tubeOffset[i],
+                        topTube.getWidth(), topTube.getHeight());
+                bottomTubeRectangles[i] = new Rectangle(tubeX[i],Gdx.graphics.getHeight()/2 - gap/2 - bottomTube.getHeight(),
+                        bottomTube.getWidth(), bottomTube.getHeight());
+
             }
             if(birdY > 0 || velocity < 0) {
                 velocity += gravity;
@@ -85,6 +101,17 @@ public class BlindBird extends ApplicationAdapter {
 
         batch.draw(birds[flapState], birdX, birdY);
         batch.end();
+        birdCircle.set(Gdx.graphics.getWidth()/2, birdY + birds[flapState].getHeight()/2,
+                birds[flapState].getWidth()/2);
+//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+//        shapeRenderer.setColor(Color.BLACK);
+//        shapeRenderer.circle(birdCircle.x,birdCircle.y, birdCircle.radius);
+        for(int i = 0; i < noOfTubes; i++){
+            if(Intersector.overlaps(birdCircle, topTubeRectangles[i]) ||
+                    Intersector.overlaps(birdCircle, bottomTubeRectangles[i])){
+            }
+        }
+//        shapeRenderer.end();
 
 	}
 	
